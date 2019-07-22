944de0499346f16ae073ed2b92e9290417b81982
Your NameDmitry <dima.klimov.98@inbox.ru>
2019-07-23 00:16:25 +0300

    work

--- src/main/java/Constants.java
+++
@@ -1,5 +0,0 @@
-public class Constants {
-    public static final String URL_CONNECTION_DB = "jdbc:h2:~/h2db";
-    public static final String NAME_CONNECTION_DB = "tully";
-    public static final String PASSWORD_CONNECTION_DB = "tully";
-}
