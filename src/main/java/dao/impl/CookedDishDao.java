be4a4d35235732cd4cf239dcf102e1e7ded9c027
Your NameDmitry <dima.klimov.98@inbox.ru>
2019-07-29 23:46:24 +0300

    CookedDishDao

--- src/main/java/dao/impl/CookedDishDao.java
+++ src/main/java/dao/impl/CookedDishDao.java
@@ -1,4 +1,55 @@
 package dao.impl;
 
-public class CookedDishDao {
+import dao.Dao;
+import dto.CookedDish;
+import org.hibernate.Hibernate;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+public class CookedDishDao implements Dao<CookedDish,Integer> {
+
+    private final SessionFactory factory;
+
+    public CookedDishDao(SessionFactory factory) {
+        this.factory = factory;
+    }
+
+    @Override
+    public void add(CookedDish cookedDish) {
+        try (Session session = factory.openSession()) {
+            session.beginTransaction();
+            session.save(cookedDish);
+            session.getTransaction().commit();
+        }
+    }
+
+    @Override
+    public CookedDish findById(Integer id) {
+        try (Session session = factory.openSession()) {
+            CookedDish dish = session.get(CookedDish.class, id);
+            if (dish != null){
+                Hibernate.initialize(dish.getEmployee());
+                Hibernate.initialize(dish.getOrder().getEmployee());
+            }
+            return dish;
+        }
+    }
+
+    @Override
+    public void update(CookedDish cookedDish) {
+        try (Session session = factory.openSession()) {
+            session.beginTransaction();
+            session.update(cookedDish);
+            session.getTransaction().commit();
+        }
+    }
+
+    @Override
+    public void delete(CookedDish cookedDish) {
+        try (Session session = factory.openSession()) {
+            session.beginTransaction();
+            session.delete(cookedDish);
+            session.getTransaction().commit();
+        }
+    }
 }
